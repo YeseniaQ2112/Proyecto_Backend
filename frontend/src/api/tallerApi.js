a5a2c4b5af461e18d8e667fa038c9f881739ee08
Laboratorio <yeseniaq7565@gmail.com>
2026-06-08 23:08:48 -1200

    Configurar API para producción

--- frontend/src/api/tallerApi.js
+++ frontend/src/api/tallerApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = '/api/taller'
+const API = `${import.meta.env.VITE_API_URL}/api/taller`
 
 export const getTalleres = async () => {
   const { data } = await axios.get(API)
